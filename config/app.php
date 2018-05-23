eafefce1c0bc3422aea3547715b841291be9c3d9
Fernando <fmti91@hotmail.com>
2018-05-23 09:37:38 -0300

    l5 repository

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //l5 repository
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+
     ],
 
     /*
